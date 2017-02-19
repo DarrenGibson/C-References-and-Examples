253877d90321f59c2276a6dc99d6cb20d2501da6
DarrenGibson <darrenGibson383@gmail.com>
2017-02-19 11:46:19 -0800

    Add files via upload

---
+++ C Output main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+int main (){
+	
+	
+	puts("Hello World"); // Standard first program output test.
+	printf("This is another way ");
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
